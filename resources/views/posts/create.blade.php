c86e54f966854913c7043b4919b33a9d5f648b80
andregds85@gmail.com <andregds85@gmail.com>
2020-10-24 01:20:08 -0300

    Retirado deste Tutorial
    https://codingdriver.com/laravel-8-crud-app-example-tutorial.html

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layout.app')
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Adicionar um novo Registro</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('posts.index') }}"> Voltar</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('posts.store') }}" method="POST">
+    @csrf
+
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Titulo</strong>
+                <input type="text" name="title" class="form-control" placeholder="Titulo">
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+              <strong>Descrição:</strong>
+                <textarea class="form-control" style="height:150px" name="description" placeholder="Descrição"></textarea>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+    </div>
+</form>
+@endsection
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layout.app')
+@section('content')
+
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Registros Gravados no Banco de Dados </h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('posts.create') }}"> Novo Registro</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered">
+        <tr>
+            <th>Titulo</th>
+            <th>Descrição</th>
+        </tr>
+        @foreach ($posts as $post)
+        <tr>
+            <td>{{ $post->title }}</td>
+            <td>{{ $post->description }}</td>
+            <td>
+                <form action="{{ route('posts.destroy',$post->id) }}" method="POST">
+                    <a class="btn btn-info" href="{{ route('posts.show',$post->id) }}">Listar</a>
+                    <a class="btn btn-primary" href="{{ route('posts.edit',$post->id) }}">Editar</a>
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Apagar</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+
+@endsection
